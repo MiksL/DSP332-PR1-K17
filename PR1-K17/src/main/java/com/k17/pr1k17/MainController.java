8dd93a7b980845e590a2c8045c022609a3b3132f
Egija Kokorevica <egija124@inbox.lv>
2024-03-16 02:08:48 +0200

    Sum function added

--- PR1-K17/src/main/java/com/k17/pr1k17/MainController.java
+++ PR1-K17/src/main/java/com/k17/pr1k17/MainController.java
@@ -1,11 +1,9 @@
 package com.k17.pr1k17;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.*;
 import javafx.collections.FXCollections;
-import javafx.scene.control.Slider;
+import javafx.scene.input.KeyCode;
 import java.util.Random;
 
 
@@ -35,8 +33,7 @@ public class MainController {
     private Label bankPoints;
 
     @FXML
-    private Button startButton;
-
+    private TextField numberSum;
 
 
     @FXML
@@ -49,6 +46,13 @@ public class MainController {
         slider.valueProperty().addListener((observable, oldValue, newValue) -> {
             sliderValue.setText("Slider Value: " + (int) slider.getValue());
         });
+
+        numberSum.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                handleTextFieldInput();
+            }
+        });
+
     }
 
     @FXML
@@ -70,7 +74,74 @@ public class MainController {
         whoStarts.setDisable(true);
         slider.setDisable(true);
         OkButton.setDisable(true);
+
+        // numberSum text field becomes visible
+        numberSum.setVisible(true);
     }
+
+    @FXML
+    // Sum function
+    private void handleTextFieldInput() {
+        // Get the input from the text field
+        String input = numberSum.getText();
+
+        // Split the input string by '+' to extract individual numbers
+        String[] numbersToSum = input.split("\\+");
+
+        // Check if there's only one number entered
+        if (numbersToSum.length < 2) {
+            return;
+        }
+
+        int sum = 0;
+
+        // Calculate the sum of the numbers
+        for (String number : numbersToSum) {
+            sum += Integer.parseInt(number.trim());
+        }
+
+        String originalString = numbers.getText();
+        String concatenatedNumbers = input.replace("+", "");
+
+        // Check if the entered numbers match the concatenated numbers
+        if (originalString.contains(concatenatedNumbers)) {
+
+            // If the sum is less than 7, increment total points by 1
+            if (sum > 7) {
+                int totalPointsValue = Integer.parseInt(totalPoints.getText());
+                totalPoints.setText(String.valueOf(totalPointsValue + 1));
+
+            } else if (sum < 7) {
+                // If the sum is less than 7, deduct 1 point from total points
+                int totalPointsValue = Integer.parseInt(totalPoints.getText());
+                if (totalPointsValue > 0) {
+                    totalPoints.setText(String.valueOf(totalPointsValue - 1));
+
+                } else {
+                    // Ensure total points cannot go negative
+                    totalPoints.setText("0");
+                }
+
+            } else {
+                // If the sum is equal to 7, add 1 point to bankPoints
+                int bankPointsValue = Integer.parseInt(bankPoints.getText());
+                bankPoints.setText(String.valueOf(bankPointsValue + 1));
+            }
+
+            // Update numbers string
+            if (sum < 7) {
+                originalString = originalString.replaceFirst(concatenatedNumbers, "3");
+            } else if (sum == 7) {
+                originalString = originalString.replaceFirst(concatenatedNumbers, "2");
+            } else {
+                originalString = originalString.replaceFirst(concatenatedNumbers, "1");
+            }
+            numbers.setText(originalString);
+        }
+        numberSum.clear();
+    }
+
+
 
 
 }
